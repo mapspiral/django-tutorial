41f4b4f11619fe5a1daa1b5507b0687e209e9035
Robert van Drunen <robert@mapspiral.net>
2025-01-19 21:25:55 +0000

    feat: add views for polls application

--- apps/polls/views.py
+++ apps/polls/views.py
@@ -3,3 +3,15 @@ from django.http import HttpRequest, HttpResponse
 
 def index(request: HttpRequest):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}.")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
